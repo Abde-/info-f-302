ba0a86b42f3f461723958c3520d2d916ad67a046
Abde El-Haman <aelhaman@ulb.ac.be>
2017-04-21 00:42:57 +0200

    [ADD] ChocoSolver rajouté et parametré

--- CSPSolver/src/CSPSolver.java
+++
@@ -1,6 +0,0 @@
-
-public class CSPSolver {
-	public static void main (String[] args){
-		System.out.println("quelque chose");
-	}
-}
---
+++ CSPSolver/src/infof302/CSPSolver.java
@@ -0,0 +1,7 @@
+package infof302;
+
+public class CSPSolver {
+	public static void main (String[] args){
+		System.out.println("quelque chose");
+	}
+}
